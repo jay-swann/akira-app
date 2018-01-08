caa66658548fdfa361bbd4df0c0913333c0ea16b
James <j.swann@mail.utoronto.ca>
2018-01-08 13:58:53 -0500

    Updated logos and displaying JSON data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './akira-logo.jpg';
 import './App.css';
+var moment = require('moment');
 
 class App extends Component {
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		data: []
+  	}
+  }
+
+  getData() {
+    return fetch('https://app.akira.md/api/system_status')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({data: responseJson});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  componentDidMount() {
+    return fetch('https://app.akira.md/api/system_status')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({data: [responseJson]});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        	<header className="App-header">
+          		<img src={logo} className="App-logo" alt="logo" />
+          		<h1 className="App-title">Welcome to Akira!</h1>
+        	</header>
+        	<div>
+        		{this.state.data.map(function(item) {
+        			return (
+        				<div>
+        				   <p>Current system time: {moment(item.system_time).format("dddd MMMM D h:mm:ssA")}</p>
+        					<p>We are {(item.online) ? 'open!' : 'closed.'}</p>
+        					<p>Hours of operation: {moment(item.open_hours_today.open_at).format("h:mmA")} to {moment(item.open_hours_today.close_at).format("h:mmA")}</p>
+        				</div>
+        			)
+        		})}
+        	</div>
       </div>
     );
   }
